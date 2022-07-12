7fdcbd54e68d656f75cffc667c5194b6bd6ed86f
Cole <propertyisrobbery@hotmail.com>
2022-07-12 19:55:40 -0400

    add form clear

--- script.js
+++ script.js
@@ -5,34 +5,40 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.Book.info = function () {
-  //   return `${this.title} by ${this.author}, ${this.pages} pages, ${read}`;
-  // };
 }
-let modal = document.querySelector(".modal");
+let modal = document.querySelector('.modal');
 
-const newButton = document.querySelector(".new-btn");
-newButton.addEventListener("click", showModal);
+const newButton = document.querySelector('.new-btn');
+newButton.addEventListener('click', showModal);
 //function to show modal
 function showModal() {
-  modal.style.display = "block";
+  modal.style.display = 'block';
 }
-let closeButton = document.querySelector(".close-button");
-closeButton.addEventListener("click", hideModal);
+let closeButton = document.querySelector('.close-button');
+closeButton.addEventListener('click', hideModal);
 //function to hideModal
 function hideModal() {
-  modal.style.display = "none";
+  resetForm();
+  modal.style.display = 'none';
 }
-let submit = document.querySelector(".submit");
-submit.addEventListener("click", (e) => e.preventDefault());
-submit.addEventListener("click", addBookToLibrary);
-submit.addEventListener("click", hideModal);
-// stores new book objects into myLibrary array:
+let myForm = document.getElementById('form');
+function resetForm() {
+  myForm.reset();
+}
+
+//listeners for submit button
+//first prevents button from attempting to submit form (avoiding page refresh)
+let submit = document.querySelector('.submit');
+submit.addEventListener('click', (e) => e.preventDefault());
+submit.addEventListener('click', addBookToLibrary);
+submit.addEventListener('click', resetForm());
+submit.addEventListener('click', hideModal);
+// stores new book objects into myLibrary array using values taken from modal form:
 function addBookToLibrary() {
-  title = document.querySelector("#title").value;
-  author = document.querySelector("#author").value;
-  pages = document.querySelector("#pages").value;
-  if (document.getElementById("read").checked) {
+  title = document.querySelector('#title').value;
+  author = document.querySelector('#author').value;
+  pages = document.querySelector('#pages').value;
+  if (document.getElementById('read').checked) {
     read = true;
   } else read = false;
   let newBook = new Book(title, author, pages, read);
