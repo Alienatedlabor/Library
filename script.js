505a9b8136f872121ac9c5af5941302b8adc7d5a
Cole <propertyisrobbery@hotmail.com>
2022-07-12 21:11:46 -0400

    add broken library display

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ function resetForm() {
 let submit = document.querySelector('.submit');
 submit.addEventListener('click', (e) => e.preventDefault());
 submit.addEventListener('click', addBookToLibrary);
-submit.addEventListener('click', resetForm());
+submit.addEventListener('click', displayLibrary);
+submit.addEventListener('click', resetForm);
 submit.addEventListener('click', hideModal);
 // stores new book objects into myLibrary array using values taken from modal form:
 function addBookToLibrary() {
@@ -44,4 +45,15 @@ function addBookToLibrary() {
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
   console.log(myLibrary);
+}
+container = document.querySelector('.container');
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    container.append(card);
+    card.append(myLibrary);
+  }
+  //Look back at grid to see how to reset cards in displayLibrary
+  //if i < 1 don't clear html elements
 }
