1ce9c810f7eb14c7487e580eb2e3656bb3abdf81
Cole <propertyisrobbery@hotmail.com>
2022-08-01 20:28:09 -0400

    refactor book object to es6 class

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let myLibrary = [];
 //constructor:
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 let modal = document.querySelector('.modal');
 
